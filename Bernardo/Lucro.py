364a2f89d05052b35cd5be6889f9afbc3ff5f5a2
ernopolis <ernopolis@tutamail.com>
2025-08-23 15:17:07 -0300

    if & else. First time

--- Bernardo/Lucro.py
+++ Bernardo/Lucro.py
@@ -1,7 +1,11 @@
+qpc = float(input("Quantas peças você comprou para este mês?:"))
 qpv = float(input("Quantas peças foram vendidas este mês?:"))
 nf = float(input("Quantos funcionários você tem?:"))
-gm = qpv * 5
-v = qpv * 7
-gf = nf * 2000
-L = v - gf - gm - 1400
-print(f"Este mês você obteve um lucro de {L} reais.")
+gm = qpc * 5  # gastos com mercadoria
+v = qpv * 7  # total de vendas no mês
+gf = nf * 2000  # gastos com funcionários
+L = v - gf - gm - 1400  # Lucro
+if qpc < qpv:
+    print("Não é possivel vender mais mercadoria do que comprou")
+else:
+    print(f"Este mês você obteve um lucro de {L} reais.")
