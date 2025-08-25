52fde665eb6d5ac011ef6848ee0603409a953344
ernopolis <ernopolis@tutamail.com>
2025-08-25 20:54:50 -0300

    Just python formatting

--- UNI/exercício-3.py
+++ UNI/exercício-3.py
@@ -1,6 +1,4 @@
-
-h=float(input('Insira sua altura em metros:'))
-p=float(input('Insira o seu peso em kilos:'))
-imc=p/h**2
-print(f'Seu IMC é de {imc}.')
-
+h = float(input("Insira sua altura em metros:"))
+p = float(input("Insira o seu peso em kilos:"))
+imc = p / h**2
+print(f"Seu IMC é de {imc}.")
--- UNI/exercício-5.py
+++ UNI/exercício-5.py
@@ -1,4 +1,4 @@
-n1=float(input('Insira a nota 1: '))*2
-n2=float(input('Insira a nota 2: '))*3
-media=(n1+n2)/5
-print(f'A média ponderada de suas notas resulta em: {media}.')
+n1 = float(input("Insira a nota 1: ")) * 2
+n2 = float(input("Insira a nota 2: ")) * 3
+media = (n1 + n2) / 5
+print(f"A média ponderada de suas notas resulta em: {media}.")
