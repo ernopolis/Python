5855a530d2c1bcee9f9972ec8e39718a2041ad99
ernopolis <ernopolis@tutamail.com>
2025-09-03 08:57:18 -0300

    formatação

--- UNI/ensinando if & else.py
+++ UNI/ensinando if & else.py
@@ -1,17 +1,18 @@
 #    x = int(input('digite um número:'))
 #        p = x**2
 #    print(f'o valor é {p}')
-#exemplo de erro de indentação
+# exemplo de erro de indentação
 
 #   if condição :
 #       comandos...
-#exemplo de indentação com 'if'
+# exemplo de indentação com 'if'
 
 #    if 5==4 :
 #        print('teste 1')
 #    print('teste 2')
 
-nome = input('Digite o nome do seu time: ')
-if nome == 'galo' :
-    print('Melhor time do mundo!')
-else : print('melhora')
+nome = input("Digite o nome do seu time: ")
+if nome == "galo":
+    print("Melhor time do mundo!")
+else:
+    print("melhora")
--- UNI/exercício-12.py
+++ UNI/exercício-12.py
@@ -1,6 +1,7 @@
-a = int(input('Insira o tamanho do lado a: '))
-b = int(input('Insira o tamanho do lado b: '))
-c = int(input('Insira o tamanho do lado c: '))
-if a+b>c and a+c>b and b+c>a:
-    print('A medidados lados inseridos formarão um triângulo!') 
-else: print('As medidas inseridas não formarão um triângulo')
+a = int(input("Insira o tamanho do lado a: "))
+b = int(input("Insira o tamanho do lado b: "))
+c = int(input("Insira o tamanho do lado c: "))
+if a + b > c and a + c > b and b + c > a:
+    print("A medida dos lados inseridos formarão um triângulo!")
+else:
+    print("As medidas inseridas não formarão um triângulo")
