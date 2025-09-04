74d236bc6eb8c3e1d2679bfcccd52fe92123d565
ernopolis <ernopolis@tutamail.com>
2025-09-04 17:13:30 -0300

    pares

--- UNI/exercício-15.py
+++ UNI/exercício-15.py
@@ -1,10 +1,10 @@
-print('Insira dois números de 0 a 10')
-n1 = float(input('1º número: '))
-n2 = float(input('2º número: '))
+print("Insira dois números de 0 a 10")
+n1 = float(input("1º número: "))
+n2 = float(input("2º número: "))
 if (n1 and n2 > 10) or (n1 and n2 < 0):
-    print('Você não inseriu um número de 0 a 10')
+    print("Você não inseriu um número de 0 a 10")
 else:
-    s = n1+n2
-    m = n1*n2
+    s = n1 + n2
+    m = n1 * n2
     p = n1**n2
-    print(f'{n1} + {n2} = {s}\n{n1} x {n2} = {m}\n{n1} ^ {n2} = {p}')
+    print(f"{n1} + {n2} = {s}\n{n1} x {n2} = {m}\n{n1} ^ {n2} = {p}")
--- UNI/exercício-16.py
+++ UNI/exercício-16.py
@@ -1,10 +1,16 @@
-print('Insira três números')
-n1 = float(input('1º número: '))
-n2 = float(input('2º número: '))
-n3 = float(input('3º número: '))
-if n1 % 2 = 0:
-    
-if n2 % 2 = 0:
-
-if n3 % 2 = 0:
-
+print("Insira três números")
+n1 = float(input("1º número: "))
+n2 = float(input("2º número: "))
+n3 = float(input("3º número: "))
+pares = 0
+if n1 % 2 == 0:
+    pares = pares + 1
+if n2 % 2 == 0:
+    pares = pares + 1
+if n3 % 2 == 0:
+    pares = pares + 1
+if pares == 1:
+    p = str("par")
+else:
+    p = str("pares")
+print(f"Há {pares} {p} entre os números inseridos.")
